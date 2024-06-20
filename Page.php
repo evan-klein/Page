1ede8e9103ebb3e51f9607d6b88f23774614267d
Evan Klein <me@evanklein.com>
2024-06-20 10:02:20 -0700

    sl: progress commit

--- Page.php
+++ Page.php
@@ -5,45 +5,36 @@ namespace evan_klein\page;
 class Page {
 	// Default values
 	private $cfg = [
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL,
-		'' => NULL
+		'manifest' => NULL,
+		'lang' => 'en-US',
+
+		'title' => NULL,
+		'description' => NULL,
+		'keywords' => NULL,
+
+		'shortcut icon' => NULL,
+		'icon' => NULL,
+		'apple-touch-icon' => NULL,
+		'apple-touch-icon-precomposed' => NULL,
+
+		'preconnect' => [],
+
+		'viewport' => 'width=device-width, user-scalable=no, initial-scale=1.0',
+		'css' => [],
+
+		'js' => [],
+		'templates' => [],
+		'noscript' => NULL,
+
+		'plausible' => NULL,
+
+		'custom' => NULL,
+
+		'og:title' => NULL,
+		'og:image' => NULL,
+		'rss' => [],
+
+		'canonical' => NULL
 	];
 
 
